9e296ce1eb91cd28df5737568c299a5068d400bc
rachosuar <rachosuar@gmail.com>
2022-12-11 00:22:59 -0300

    contract and API update!

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+export const contractAddress = "0x251931838a20CC4343d1B6245039f2F9FE4E82FF";
 export const contractABI = abi.abi;
